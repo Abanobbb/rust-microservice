80fd0febc68a59d441e0662831107314c8a460a8
abanoubL <abanoub.twadros@gmail.com>
2023-10-24 23:20:19 +0300

    added health check for the auth

--- src/health-check-service/main.rs
+++ src/health-check-service/main.rs
@@ -3,10 +3,9 @@ use std::env;
 use authentication::auth_client::AuthClient;
 use authentication::{SignInRequest, SignOutRequest, SignUpRequest};
 use tokio::time::{sleep, Duration};
-use tonic::{Request, Response};
 use uuid::Uuid;
 
-use crate::authentication::{StatusCode, SignUpResponse, SignInResponse, SignOutResponse};
+use crate::authentication::StatusCode;
 
 pub mod authentication {
     tonic::include_proto!("authentication");
@@ -14,22 +13,21 @@ pub mod authentication {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // AUTH_SERVICE_HOST_NAME will be set to 'auth' when running the health check service in Docker
-    // ::0 is required for Docker to work: https://stackoverflow.com/questions/59179831/docker-app-server-ip-address-127-0-0-1-difference-of-0-0-0-0-ip
     let auth_hostname = env::var("AUTH_SERVICE_HOST_NAME").unwrap_or("[::0]".to_owned());
 
-    // Establish connection when auth service
     let mut client = AuthClient::connect(format!("http://{}:50051", auth_hostname)).await?;
 
     loop {
-        let username: String = todo!(); // Create random username using new_v4()
-        let password: String = todo!(); // Create random password using new_v4()
+        let username = Uuid::new_v4().to_string();
+        let password = Uuid::new_v4().to_string();
 
-        let request: Request<SignUpRequest> = todo!(); // Create a new `SignUpRequest`.
+        let request = tonic::Request::new(SignUpRequest {
+            username: username.clone(),
+            password: password.clone(),
+        });
 
-        let response: Response<SignUpResponse> = todo!(); // Make a sign up request. Propagate any errors.
+        let response = client.sign_up(request).await?;
 
-        // Log the response
         println!(
             "SIGN UP RESPONSE STATUS: {:?}",
             StatusCode::from_i32(response.into_inner().status_code)
@@ -37,25 +35,29 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         // ---------------------------------------------
 
-        let request: Request<SignInRequest> = todo!(); // Create a new `SignInRequest`.
+        let request = tonic::Request::new(SignInRequest {
+            username: username.clone(),
+            password: password.clone(),
+        });
 
-        // Make a sign in request. Propagate any errors. Convert Response<SignInResponse> into SignInResponse.
-        let response: SignInResponse = todo!();
+        let response = client.sign_in(request).await?.into_inner();
 
         println!(
             "SIGN IN RESPONSE STATUS: {:?}",
-            todo!() // Log response status_code
+            StatusCode::from_i32(response.status_code)
         );
 
         // ---------------------------------------------
 
-        let request: Request<SignOutRequest> = todo!(); // Create a new `SignOutRequest`.
+        let request = tonic::Request::new(SignOutRequest {
+            session_token: response.session_token,
+        });
 
-        let response: Response<SignOutResponse> = todo!(); // Make a sign out request. Propagate any errors.
+        let response = client.sign_out(request).await?;
 
         println!(
             "SIGN OUT RESPONSE STATUS: {:?}",
-            todo!() // Log response status_code
+            StatusCode::from_i32(response.into_inner().status_code)
         );
 
         println!("--------------------------------------",);
